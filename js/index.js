09a2a5cfc2692eb81265e9fd49d238307d7faef4
MariaB <lvlaria.15@gmail.com>
2023-07-28 22:24:45 +0300

    калькулятор считает и выводит результат Поле ввода и кнопки (не все еще) стали активны

--- js/index.js
+++ js/index.js
@@ -2,71 +2,163 @@ import { changeColor } from "./toggleButton.js";
 let toggleButton = document.querySelector("#toggleButton");
 toggleButton.addEventListener("click", changeColor);
 
-// let buttons = document.querySelectorAll(".button");
-// buttons.forEach(function (button) {
-//   button.addEventListener("click", buttonClicked);
-// });
-
-let input = document.querySelector(".display__text_result");
+let displayState = document.getElementById("displayState");
+let display = "";
 let keybord = document.querySelector(".keybord");
-keybord.addEventListener("click", buttonClicked);
+keybord.addEventListener("click", buttonClick);
 
-function buttonClicked(e) {
-  let activeButton = e.target.closest("button");
-  if (!activeButton) {
-    return;
+function buttonClick(e) {
+  let action = e.target.dataset.action;
+
+  switch (action) {
+    case "one":
+      addNumber(1);
+      break;
+
+    case "two":
+      addNumber(2);
+      break;
+
+    case "three":
+      addNumber(3);
+      break;
+
+    case "four":
+      addNumber(4);
+      break;
+
+    case "five":
+      addNumber(5);
+      break;
+
+    case "six":
+      addNumber(6);
+      break;
+
+    case "seven":
+      addNumber(7);
+      break;
+
+    case "eight":
+      addNumber(8);
+      break;
+
+    case "nine":
+      addNumber(9);
+      break;
+
+    case "zero":
+      addNumber(0);
+      break;
+
+    case "minus":
+      addOperator("-");
+      break;
+
+    case "plus":
+      addOperator("+");
+      break;
+
+    case "division":
+      addOperator("/");
+      break;
+
+    case "multipli":
+      addOperator("*");
+      break;
+
+    case "clear":
+      clearAll();
+      break;
+
+    case "percent":
+      addPercent("%");
+      break;
+
+    case "delete_element":
+      deleteElement();
+      break;
+
+    case "dot":
+      addDot(".");
+      break;
+
+    case "equally":
+      calculate();
+      break;
+
+    default:
+      break;
   }
-  const buttonValue = activeButton.innerText;
-
-  // switch(value){
-  //     case'C':
-  //     input.innerText = '';
-  //     return;
-  //   }
-
-  // if (buttonValue === "C") {
-  //   input.innerText = "";
-  //   return;
-  // }
-
-  if (e.target.dataset.clearAll != undefined) {
-    input.innerText = "";
-    return;
-  }
-
-  // if (activeButton.classList.contains("icon")) {
-  //   input.innerText = input.innerText.substring(0, input.innerText.length - 1);
-  //   return;
-  // }
-
-  if (e.target.dataset.deleteElement != undefined) {
-    input.innerText = input.innerText.substring(0, input.innerText.length - 1);
-    return;
-  }
-
-  // if (buttonValue === "0" && input.innerText === "0") {
-  //   return;
-  // }
-
-  // if (e.target.dataset.zero != undefined && input.innerText.dataset.zero != undefined){
-  //   return;
-  // }
-
-  
-
-  if (input.innerText === "0") {
-    input.innerText = "";
-  }
-
-  input.innerHTML += buttonValue;
-
-  console.log(activeButton);
+  showDisplay();
 }
 
-// const icon = document.querySelector(".button__icon");
-//   icon.addEventListener("click", deleteElement);
+function showDisplay() {
+  displayState.innerText = display;
+}
 
-// function deleteElement() {
-//   let text = input.innerText;
-//   input.innerText = text.substring(0, text.length -1);
-// }
+function clearAll() {
+  display = "";
+}
+
+function deleteElement() {
+  display = display.substring(0, display.length - 1);
+  console.log(deleteElement);
+}
+
+function isOperator(value) {
+  return value === "-" || value === "+" || value === "/" || value === "*";
+}
+
+function addOperator(value) {
+  const lastElement = display[display.length - 1];
+  const isLastElementOperator = isOperator(lastElement);
+
+  if (lastElement === ".") {
+    display = display.slice(0, -1);
+  }
+
+  if (isLastElementOperator) {
+    const newdisplayState = display.slice(0, -1) + "";
+    console.log(newdisplayState);
+    display = newdisplayState + " " + value;
+    return;
+  } else {
+    display = display + " " + value;
+  }
+}
+
+function addNumber(value) {
+  const lastElement = display[display.length - 1];
+  const isLastElementOperator = isOperator(lastElement);
+
+  if (display === "0") {
+    display = value + "";
+    return;
+  }
+
+  if (isLastElementOperator) {
+    display = display + " " + value;
+  } else {
+    display = display + value;
+  }
+}
+
+function addDot() {
+  const lastElement = display[display.length - 1];
+  const isLastElementOperator = isOperator(lastElement);
+  if (isLastElementOperator || lastElement === "." || display === "") {
+    return;
+  } else {
+    display = display + ".";
+  }
+}
+
+function addPercent() {
+  display = eval(display) / 100;
+}
+
+function calculate() {
+  const computedResult = display.split(" ").join("");
+  display = eval(computedResult);
+}
